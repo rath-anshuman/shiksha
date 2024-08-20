6d92b4922f9d9c13b6e01d1a4fffc920b9317b0f
Anshuman Rath <anshumanrath030@gmail.com>
2024-08-20 14:47:17 +0530

    .

--- tadmin/urls.py
+++ tadmin/urls.py
@@ -4,6 +4,5 @@ from .views import routine,routinedtl,list_routine
 
 urlpatterns = [
     path('routine/',routine),
-    path('routine/l/',list_routine),
     path('routine/<int:pk>',routinedtl),
 ]
