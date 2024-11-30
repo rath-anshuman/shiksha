c510ae5ee984978373306370f36c64cf3654ab4e
Anshuman Rath <anshumanrath030@gmail.com>
2024-11-30 20:36:14 +0530

    .

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import UserAccount
 
-class UserAccountAdmin(UserAdmin):
-    model = UserAccount
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
-    search_fields = ('username', 'email')
-    ordering = ('username',)
-    filter_horizontal = ('groups', 'user_permissions')
-
-admin.site.register(UserAccount, UserAccountAdmin)
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'regno', 'section', 'email', 'is_active', 'is_staff')
+    list_filter = ('section', 'is_active', 'is_staff')
+    ordering = ('regno',)
--- Accounts/models.py
+++ Accounts/models.py
@@ -4,34 +4,18 @@ from .manager import AccountManager
 from django.utils import timezone
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(blank=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
+    name = models.CharField(max_length=30, blank=True)
+    regno = models.CharField(max_length=15, unique=True)
+    section = models.CharField(max_length=5, blank=True)
+    email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
-    date_joinee = models.DateTimeField(default=timezone.now)
-    # No need to redefine password field, as AbstractBaseUser handles it
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='custom_user_accounts',
-        blank=True,
-        help_text='The groups this user belongs to.',
-        verbose_name='groups'
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='custom_user_permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        verbose_name='user permissions'
-    )
 
     objects = AccountManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'regno'  
+    REQUIRED_FIELDS = ['email', 'name']
 
     def __str__(self):
-        return self.username
+        return self.regno
